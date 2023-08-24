7ab9717923c75df48440063fcb6d9aaab40ef134
Lorenz Kort <lorenz.kort@gmail.com>
2023-08-24 18:15:29 +0200

    changed from selenium to requests lib

--- manage.py
+++ manage.py
@@ -2,17 +2,20 @@ from datetime import datetime
 from objects import get_pararius_objects
 from telegram import send_text
 
-fresh_objects = get_pararius_objects(url='https://www.pararius.com/apartments/haarlem/0-1300')
-
-def init_file(fresh_objects):
-    objs = [f'{o}, {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}\n' for o in fresh_objects]
-    with open('link_file.csv', 'w+') as f:
-        f.write('link,timestamp\n')
-        for o in objs:
-            f.write(o)
-    return
-
-def cronjob():
+def cronjob(city='haarlem', minimum_bedrooms='1', max_price='1500', km_radius='10'):
+    
+    # build URL from params
+    city = '/' + city if city else ''
+    minimum_bedrooms = '/' + minimum_bedrooms if minimum_bedrooms else ''
+    max_price = '/' + max_price if max_price else ''
+    km_radius = '/' + km_radius if km_radius else ''
+    
+    
+    url = f'https://www.pararius.com/apartments{city}{minimum_bedrooms}{max_price}{km_radius}'
+    
+    
+    fresh_objects = get_pararius_objects(url=url)
+    
     # Get new items
     unkown_objects = [obj for obj in fresh_objects if obj not in open('link_file.csv', 'r').read()]
     
--- objects.py
+++ objects.py
@@ -1,30 +1,18 @@
 import time
 from bs4 import BeautifulSoup as bs
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from pyvirtualdisplay import Display
+import requests as r
 
 
  
 # Returns the URL's that are currently on the site
-def get_pararius_objects(url='', headless=False):
+def get_pararius_objects(url='https://www.pararius.com/apartments/haarlem/0-1300', headless=False):
+    
     # Call as: get_pararius_objects('https://www.pararius.com/apartments/haarlem/0-1300')
-
-    # Create driver
-    options = Options()
-    options.headless = headless
-    driver = webdriver.Chrome(options=options) # '/usr/lib/chromium-browser/chromedriver'
-    driver.implicitly_wait(10)
-
-
-    # Get HTML from site and quit driver
-    driver.get(url)
-    time.sleep(30)
-    html = driver.page_source
-    driver.quit()
+    html = r.get(url).text
 
     # search for all HTML list-items with class search-list__item search-list__item--listing
     soup = bs(html, 'html.parser')
     items = soup.find_all("a", "listing-search-item__link listing-search-item__link--title", href=True)
     parsed_items = ['https://pararius.com' + a['href'] for a in items]
+    
     return parsed_items # Returns the URL's that are currently on the site
